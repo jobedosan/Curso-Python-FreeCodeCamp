a4114500504dea97fe1ef45cbd8d720eefe3c5c5
José Domador <83101987+jobedosan@users.noreply.github.com>
2023-06-03 19:34:21 -0400

    Tipos de datos: Enteros, decimales y booleanos

---
+++ python/tipo-de-datos.py
@@ -0,0 +1,14 @@
+#Datos numéricos (Números enteros)
+print(type(10))
+print(type(-15))
+print(type(0))
+
+#Datos numéricos (Números en coma flotante)
+print(type(0.5))
+print(type(0.0))
+print(type(-4.7))
+
+#Datos booleanos (True o False)
+print(type(True))
+print(type(False))
+
