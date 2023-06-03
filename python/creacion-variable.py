74be345df17dffec7427683356032a1b81762da0
José Domador <83101987+jobedosan@users.noreply.github.com>
2023-06-03 00:52:58 -0400

    Variables

---
+++ python/creacion-variable.py
@@ -0,0 +1,10 @@
+#Definición de variable
+num = 15
+print(num)
+
+#Actualización de variable
+num = 8
+print(num)
+
+#Utilización de variable en una operación matemática
+print(num+2)
--- python/hola.mundo.py
+++ python/hola.mundo.py
@@ -1,1 +1,2 @@
+#Primer programa hecho con Python y la función print (muestra un texto o valor por consola)
 print("¡Hola, mundo!")
