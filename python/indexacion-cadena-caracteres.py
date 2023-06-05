44fed2e9114dcb2305a260c87a1dbc7a14d55673
José Domador <83101987+jobedosan@users.noreply.github.com>
2023-06-05 11:16:32 -0400

    índexación

---
+++ python/indexacion-cadena-caracteres.py
@@ -0,0 +1,20 @@
+#Indexación
+print("Python" [0])
+print("Python" [1])
+print("Python" [2])
+print("Python" [3])
+print("Python" [4])
+print("Python" [5])
+
+nombre = "Benjamin"
+print(nombre [0])
+print(nombre [1])
+print(nombre [2])
+print(nombre [3])
+print(nombre [4])
+print(nombre [5])
+print(nombre [6])
+print(nombre [7])
+
+#La siguiente línea provocará un error de índice (descomentar para ver el error)
+#print(nombre [8])
