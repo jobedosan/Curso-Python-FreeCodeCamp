01c41bfe92201080b241d65a8384552582a0823c
José Domador <83101987+jobedosan@users.noreply.github.com>
2023-06-09 01:44:28 -0400

    Operadores relacionales

---
+++ python/operadores-relacionales.py
@@ -0,0 +1,58 @@
+#Operadores relacionales trabajando con números
+print("Operadores relacionales trabajando con números")
+print("Resultado de menor que")
+print(5 < 10)
+print(10 < 5)
+print("\n")
+
+print("Resultado de mayor que")
+print(5 > 10)
+print(10 > 5)
+print("\n")
+
+print("Resultado de mayor o igual que")
+print(5 >= 10)
+print(10 >= 5)
+print(10 >= 10)
+
+print("\n")
+print("Resultado de menor o igual que")
+print(5 <= 10)
+print(10 <= 5)
+print(10 <= 10)
+print("\n")
+
+print("Resultado de igual que")
+print(5 == 10)
+print(10 == 10)
+print("\n")
+
+print("Resultado de diferente que")
+print(5 != 10)
+print(10 != 10)
+print("\n")
+
+#Operadores relacionales trabajando con cadenas de caracteres
+print("Operadores relacionales trabajando con cadenas de caracteres")
+print("\n")
+
+print("Resultado de menor que")
+print("ABC" < "A")
+print("A" < "B")
+print("\n")
+
+print("Resultado de mayor que")
+print("ABC" > "A")
+print("A" > "B")
+print("\n")
+
+print("Resultado de igual que")
+print("a" == "a")
+print("A" == "B")
+print("a" == "A")
+print("\n")
+
+print("Resultado de diferente que")
+print("a" != "a")
+print("A" != "B")
+print("a" != "A")
