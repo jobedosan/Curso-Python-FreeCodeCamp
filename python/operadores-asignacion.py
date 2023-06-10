84e74cc542050a73b8801e63478954e75d849101
José Domador <83101987+jobedosan@users.noreply.github.com>
2023-06-09 23:53:18 -0400

    Operadores de asignación

---
+++ python/operadores-asignacion.py
@@ -0,0 +1,55 @@
+#Operador =
+print("Resultado del =")
+variable = 20
+variable = 70
+print(variable)
+print("\n")
+
+#Operador +=
+print("Resultado del +=")
+variable = 50
+variable += 5
+print(variable)
+print("\n")
+
+#Operador -=
+print("Resultado del -=")
+variable = 50
+variable -= 5
+print(variable)
+print("\n")
+
+#Operador *=
+print("Resultado del *=")
+variable = 50
+variable *= 3
+print(variable)
+print("\n")
+
+#Operador **=
+print("Resultado del **=")
+variable = 50
+variable **= 2
+print(variable)
+print("\n")
+
+#Operador /=
+print("Resultado del /=")
+variable = 50
+variable /= 2
+print(variable)
+print("\n")
+
+#Operador //=
+print("Resultado del //=")
+variable = 50
+variable //= 2
+print(variable)
+print("\n")
+
+#Operador %=
+print("Resultado del //=")
+variable = 20
+variable %= 2
+print(variable)
+print("\n")
